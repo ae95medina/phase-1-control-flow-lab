25bfa9d0d8bdfebef945e4827e03acb4318e9f9e
ae95medina <ae95medina@gmail.com>
2022-05-01 11:22:55 -0500

    commit tree 15a020c367ca8794089151a3a21182fc4f169d2d

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(i){
+  if (i <= 400){
+    return 'This one is on me!'
+  }
+  if (i <=2000){
+    return 'That will be twenty bucks.'
+  }
+  if (i > 2000, i < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  if (i > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === 'NYC'? 'Ok, sounds good.' : 'No go.'); 
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous': {
+      return 'Thank you so much.'
+    }
+    case 'not as generous':{
+      return 'Thank you.'
+    }
+    default: {
+      return 'Bye.'
+    }
+      
+  }
   // Write your code here!
 }
